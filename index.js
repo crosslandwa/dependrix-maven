9eb59d61f67343e49ff43c3961e1248d69008d69
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-05-10 00:06:01 +0100

    Read content from supplied streams

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('dependrix-maven is go')
+const { Writable } = require('stream')
+
+function DependrixMaven (streams) {
+  return Promise.all(streams.map(
+    stream => new Promise((resolve, reject) => {
+      let content
+      const outStream = new Writable({
+        write (chunk) {
+          content = chunk.toString('utf8')
+        }
+      })
+      stream.pipe(outStream)
+      stream.on('end', () => resolve(content))
+    })
+  ))
+}
+
+module.exports = DependrixMaven
--- spec/dependrixMavenSpec.js
+++ spec/dependrixMavenSpec.js
@@ -1,5 +1,25 @@
+const DependrixMaven = require('..')
+const { Readable } = require('stream')
+
 describe('dependrix-maven', () => {
-  it('can be tested', () => {
-    expect('a').toEqual('a')
+  it('reads content from the supplied array of streams', done => {
+    DependrixMaven([
+      asStream('abc'),
+      asStream('def'),
+      asStream('xyz')
+    ])
+      .then(readContent => {
+        expect(readContent[0]).toEqual('abc')
+        expect(readContent[1]).toEqual('def')
+        expect(readContent[2]).toEqual('xyz')
+      })
+      .then(done, done.fail)
   })
 })
+
+function asStream (content) {
+  const s = new Readable()
+  s.push(content)
+  s.push(null)
+  return s
+}
