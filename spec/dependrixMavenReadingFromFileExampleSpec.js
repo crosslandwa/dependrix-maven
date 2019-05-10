734f43129c808515d319332a3f487a1a249b0af4
will.crossland <4037089+crosslandwa@users.noreply.github.com>
2019-05-10 19:25:19 +0100

    Add example spec to demonstrate streaming from a file on disk

---
+++ spec/dependrixMavenReadingFromFileExampleSpec.js
@@ -0,0 +1,51 @@
+const DependrixMaven = require('..')
+const fs = require('fs')
+
+describe('dependrix-maven', () => {
+  it('analyses a maven dependency tree read in from a file by the client', done => {
+    DependrixMaven([fs.createReadStream(`${__dirname}/example-tree.txt`, { encoding: 'utf8' })])
+      .then(expectReturnedObjectToEqual({
+        artifacts: {
+          'com.chippanfire:max.msp.bucket': {
+            groupId: 'com.chippanfire',
+            artifactId: 'max.msp.bucket',
+            version: '0.1.0',
+            dependencies: {
+              'org.testng:testng': {
+                version: '6.1.1',
+                scope: 'test'
+              },
+              'junit:junit': {
+                version: '3.8.1',
+                scope: 'test'
+              },
+              'org.beanshell:bsh': {
+                version: '2.0b4',
+                scope: 'test'
+              },
+              'com.beust:jcommander': {
+                version: '1.12',
+                scope: 'test'
+              },
+              'org.yaml:snakeyaml': {
+                version: '1.6',
+                scope: 'test'
+              }
+            }
+          }
+        },
+        dependencies: {
+          'org.testng:testng': [ '6.1.1' ],
+          'junit:junit': [ '3.8.1' ],
+          'org.beanshell:bsh': [ '2.0b4' ],
+          'com.beust:jcommander': [ '1.12' ],
+          'org.yaml:snakeyaml': [ '1.6' ]
+        }
+      }))
+      .then(done, done.fail)
+  })
+})
+
+function expectReturnedObjectToEqual (expected) {
+  return actual => expect(JSON.stringify(actual, null, 2)).toEqual(JSON.stringify(expected, null, 2))
+}
